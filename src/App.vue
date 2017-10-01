b4c1be5f0a67faa91259886aa92c86e768c68821
ThiagoCardosoo <trovador182@gmail.com>
2017-10-01 14:46:20 -0300

    estrutua inicial

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+     <h1>{{ titulo }}</h1>
+     <ul>
+       <li v-for="foto of fotos">
+          <img :src="foto.url" :alt="foto.title">
+        </li>
+     </ul>
+</div>
+</template>
+
+<script>
+export default {
+  data(){
+
+      return  {
+        titulo: 'Alurapic',
+        fotos: [
+          {
+           url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+           titulo: 'Cachorro'
+          },
+           {
+           url: 'http://tudosobrecachorros.com.br/wp-content/uploads/cachorro-independente.jpg',
+           titulo: 'Cachorro'
+          }
+        ]
+      }
+
+  }
+}
+</script>
+
+<style>
+
+</style>
